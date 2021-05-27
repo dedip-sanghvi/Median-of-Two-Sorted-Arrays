36b730da6fd90b66527bcf5da3a896f90209308c
dedip-sanghvi <dedip.san@gmail.com>
2021-05-26 20:14:51 -0700

    Create FindMedianSortedArrays.py

---
+++ FindMedianSortedArrays.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+        num_len = len(nums1)
+        if len == 0:
+            return
+        elif len == 1:
+            return nums1[0]
+        else:
+            mid = int(num_len/2)
+            if num_len%2==0:
+                return (nums1[mid]+nums1[mid-1])/2
+            else:
+                return nums1[mid]
