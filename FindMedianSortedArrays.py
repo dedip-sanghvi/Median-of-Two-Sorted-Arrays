92e9a2157c802446e7d33ad23666618fea3180c8
dedip-sanghvi <dedip.san@gmail.com>
2021-05-26 20:25:42 -0700

    Update FindMedianSortedArrays.py

--- FindMedianSortedArrays.py
+++ FindMedianSortedArrays.py
@@ -3,13 +3,8 @@ class Solution:
         nums1.extend(nums2)
         nums1.sort()
         num_len = len(nums1)
-        if num_len == 0:
-            return
-        elif num_len == 1:
-            return nums1[0]
+        mid = int(num_len/2)
+        if num_len%2==0:
+            return (nums1[mid]+nums1[mid-1])/2
         else:
-            mid = int(num_len/2)
-            if num_len%2==0:
-                return (nums1[mid]+nums1[mid-1])/2
-            else:
                 return nums1[mid]
