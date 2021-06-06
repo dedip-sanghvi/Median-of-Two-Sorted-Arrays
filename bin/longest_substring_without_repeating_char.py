c8658f3c98d52807b36023f637aedf0a6a706ec2
dedip-sanghvi <dedip.san@gmail.com>
2021-06-06 07:04:09 -0700

    Performance changes  lengthOfLongestSubstring

--- bin/longest_substring_without_repeating_char.py
+++ bin/longest_substring_without_repeating_char.py
@@ -1,30 +1,22 @@
 class Solution:
-    def __is_string_unique(self, s):
-        set_len = len({c for c in s})
-        return len(s)==set_len
     
     def lengthOfLongestSubstring(self, s: str) -> int:
         i=0
         len_s = len(s)
         max_len =0
-        if len_s==1:
-            return 1
-        elif self.__is_string_unique(s):
-            return len_s
-        else:
-            while(i<len_s):
-                j=i+1
-                sub_str = {s[i]}
-                sub_str_len=1
-                while(j<len_s):
-                    c = s[j]
-                    if c in sub_str:
-                        break
-                    else:
-                        sub_str.add(s[j])
-                        sub_str_len+=1
-                        j+=1
-                if sub_str_len>max_len:
-                    max_len=sub_str_len
-                i+=1
-            return max_len
+        while(i<len_s):
+            j=i+1
+            sub_str = {s[i]}
+            sub_str_len=1
+            while(j<len_s):
+                c = s[j]
+                if c in sub_str:
+                    break
+                else:
+                    sub_str.add(s[j])
+                    sub_str_len+=1
+                    j+=1
+            if sub_str_len>max_len:
+                max_len=sub_str_len
+            i+=1
+        return max_len
